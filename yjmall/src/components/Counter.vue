4d065e1ac9fb9d54008cdc1f772b5e08ac3bd59e
yaoyu2001 <yongchangyao2001@gmail.com>
2020-10-01 00:01:54 -0400

    2020.09.30

---
+++ yjmall/src/components/Counter.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+      <button v-on:click = "increment">+</button>
+      <button v-on:click="decrement">-</button>
+      <p><span>{{num}}</span></p>
+    </div>
+</template>
+
+<script>
+  export default {
+    props:['num'],
+    name: 'HelloWorld',
+    data () {
+      return {
+        // msg: 'Welcome to Your Vue.js App',
+        // num:0,
+      }
+    },
+    methods:{
+      increment(){
+        this.num++
+      },
+      decrement(){
+        this.num--
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ yjmall/views/Goodlist.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "Goodlist"
+    }
+</script>
+
+<style scoped>
+
+</style>
